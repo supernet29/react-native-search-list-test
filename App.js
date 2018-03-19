47bb3b9c6ed73cf09bcf06ace8c6ba954aa0bb2d
WoochanKim <woochankim200@gmail.com>
2018-03-19 19:13:42 +0900

    write test example. get test example in react-native-search-list example

--- App.js
+++ App.js
@@ -9,50 +9,128 @@ import {
   Platform,
   StyleSheet,
   Text,
+  TouchableOpacity,
+  Alert,
+  StatusBar,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import SearchList from '@unpourtous/react-native-search-list'
+import demoList from './data'
 
 type Props = {};
+const rowHeight = 40
 export default class App extends Component<Props> {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      dataSource: demoList
+    }
+  }
+
+  // custom render row
+  renderRow (item, sectionID, rowID, highlightRowFunc, isSearching) {
+    return (
+      <TouchableOpacity onPress={() => {
+        Alert.alert('Clicked!', `sectionID: ${sectionID}; item: ${item.searchStr}`,
+          [
+            {text: 'OK', onPress: () => console.log('OK Pressed')},
+          ],
+          {cancelable: true})
+      }}>
+        <View key={rowID} style={{flex: 1, marginLeft: 20, height: rowHeight, justifyContent: 'center'}}>
+          {/*use `HighlightableText` to highlight the search result*/}
+          <Text>{item.searchStr}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  // render empty view when datasource is empty
+  renderEmpty () {
+    return (
+      <View style={styles.emptyDataSource}>
+        <Text style={{color: '#979797', fontSize: 18, paddingTop: 20}}> No Content </Text>
+      </View>
+    )
+  }
+
+  // render empty result view when search result is empty
+  renderEmptyResult (searchStr) {
+    return (
+      <View style={styles.emptySearchResult}>
+        <Text style={{color: '#979797', fontSize: 18, paddingTop: 20}}> No Result For <Text
+          style={{color: '#171a23', fontSize: 18}}>{searchStr}</Text></Text>
+        <Text style={{color: '#979797', fontSize: 18, alignItems: 'center', paddingTop: 10}}>Please search again</Text>
+      </View>
+    )
+  }
+
+  render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <SearchList
+          data={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)}
+          renderEmptyResult={this.renderEmptyResult.bind(this)}
+          renderBackButton={() => null}
+          renderEmpty={this.renderEmpty.bind(this)}
+
+          rowHeight={rowHeight}
+
+          toolbarBackgroundColor={'#2196f3'}
+          title='Search List Demo'
+          cancelTitle='取消'
+          onClickBack={() => {}}
+          showToolbar={false}
+
+          searchListBackgroundColor={'#2196f3'}
+
+          searchBarToggleDuration={300}
+
+          searchInputBackgroundColor={'#0069c0'}
+          searchInputBackgroundColorActive={'#6ec6ff'}
+          searchInputPlaceholderColor={'#FFF'}
+          searchInputTextColor={'#FFF'}
+          searchInputTextColorActive={'#000'}
+          searchInputPlaceholder='Search'
+          sectionIndexTextColor={'#6ec6ff'}
+          searchBarBackgroundColor={'#2196f3'}
+        />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#efefef',
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 5
   },
-});
+  emptyDataSource: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    marginTop: 50
+  },
+  emptySearchResult: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    marginTop: 50
+  }
+})
